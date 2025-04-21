9bdb6dc27c59fe42835fed68876d37ace51cb40b
MeghaVernekar23 <meghavernekar98@gmail.com>
2025-04-21 20:29:27 +0200

    Commiting User_servcies. Added User Authentication

---
+++ service/auth.py
@@ -0,0 +1,40 @@
+from passlib.context import CryptContext
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends
+from fastapi import HTTPException, status
+
+SECRET_KEY = "secret_key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated= "auto" )
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/login")
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+def verify_password(plain_password, hash_password):
+    return pwd_context.verify(plain_password, hash_password)
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=15))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate token",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        return username
+    except JWTError:
+        raise credentials_exception
--- service/user_service.py
+++ service/user_service.py
@@ -1,10 +1,13 @@
-from db.models.pydantic_models import UsersPydantic
+from db.models.pydantic_models import UserLogin, UsersPydantic
 from sqlalchemy.orm import Session
 from typing import List
 from db.models.db_models import Users
 from db.models.pydantic_models import UsersPydantic
 from db.models.pydantic_models import UserCreatePydantic
-from exceptions import UserNotFoundException
+from exceptions import UserNotFoundException, UserAlreadyExistsException
+from fastapi.security import OAuth2PasswordRequestForm
+
+from service.auth import hash_password, create_access_token, verify_password
 
 def get_all_user(db: Session) -> List[UsersPydantic]:
     """Get list of all Users"""
@@ -28,14 +31,35 @@ def get_user_by_username(user_name: str , db: Session) -> UsersPydantic:
         raise UserNotFoundException()
     return user
 
+def get_user_by_user_email(user: OAuth2PasswordRequestForm , db: Session) -> dict:
+    """Get list of User by user name"""
+    user_detail =  db.query(Users).filter(Users.user_email == user.username).first()
 
-def create_user(user: UserCreatePydantic , db: Session) -> UsersPydantic:
+    if not user_detail or not verify_password(user.password, user_detail.password):
+        raise UserAlreadyExistsException
+    
+    access_token = create_access_token(data={"sub": user_detail.user_name})
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+def create_user(user: UserCreatePydantic , db: Session) -> dict:
     """Create users"""
-    new_user = Users(**user.model_dump())
+    existing_user = db.query(Users).filter(Users.user_email == user.user_email).first()
+
+    if existing_user:
+        raise UserAlreadyExistsException()
+    
+    hashed_pwd = hash_password(user.user_password)
+
+    new_user = Users(user_name=user.user_name,user_address=user.user_address,
+                     user_phone_no=user.user_phone_no,user_email=user.user_email,password=hashed_pwd)
+    
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
-    return new_user
+    access_token = create_access_token(data={"sub": new_user.user_name})
+
+    return {"access_token": access_token, "token_type": "bearer"}
 
 def update_user_details(user_id: int, user_data:UserCreatePydantic, db: Session)-> UsersPydantic:
     """update users"""
