877a045527f9b035059b25f1d44b3381d9bfeb00
MeghaVernekar23 <meghavernekar98@gmail.com>
2025-04-22 20:42:21 +0200

    Updated imports

--- backend/db/sessions.py
+++ backend/db/sessions.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from backend.db.models.db_models import Base
+from db.models.db_models import Base
 
 DATABASE_URL = "sqlite:///./tasktracker.db"
 engine = create_engine(DATABASE_URL, echo=True)
