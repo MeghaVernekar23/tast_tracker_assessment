eeef79a29b148738607ceac083eb800a3e8af6c9
MeghaVernekar23 <meghavernekar98@gmail.com>
2025-04-29 10:31:44 +0200

    commiting main.py. Added health check router

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from api.users import user_router
 from api.tasks import task_router
 from api.documents import document_router
+from api.health import health_router
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Task Tracker API",
@@ -19,4 +20,5 @@ app.add_middleware(
 
 app.include_router(user_router)
 app.include_router(task_router)
+app.include_router(health_router)
 app.include_router(document_router,prefix="/documents")
